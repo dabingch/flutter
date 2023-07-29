0b802526f6b27df26b385fd59d7ff5f95e8bb0b3
Hao Chen <dabingchhh@gmail.com>
2023-07-29 16:47:36 +0800

    Added button and dice with styling

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,7 +8,14 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
 
+  // * Multi constructor
+  // const GradientContainer.purple({super.key})
+  //     : color1 = Colors.deepPurple,
+  //       color2 = Colors.indigo;
+
   final List<Color> colors;
+
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -20,8 +27,23 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hi there'),
+      child: Center(
+        child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/dice-2.png', width: 200),
+              // * padding approach one
+              // const SizedBox(height: 20),
+              TextButton(
+                  onPressed: rollDice,
+                  style: TextButton.styleFrom(
+                      // * padding approach two
+                      padding: const EdgeInsets.only(top: 20),
+                      foregroundColor: Colors.white,
+                      textStyle: const TextStyle(fontSize: 28)),
+                  child: const Text('Roll Dice'))
+            ]),
       ),
     );
   }
